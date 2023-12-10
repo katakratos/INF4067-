bc5517148203f9ce214dce055960bde65759d8af
katakratos <paulemeud@gmail.com>
2023-12-10 03:54:18 +0100

    implémentation du Patron Bridge pour gérer les formes géométriques

---
+++ Patron de structuration/BRIDGE/Bridge/src/construction/pont/RedColor.java
@@ -0,0 +1,13 @@
+package construction.pont;
+
+public class RedColor implements Color{
+	
+
+	@Override
+	public void fillcolor() {
+		System.out.println("\t Red color");
+		
+		
+	}
+
+}
---
+++ Patron de structuration/BRIDGE/Bridge/src/construction/pont/Shape.java
@@ -0,0 +1,10 @@
+package construction.pont;
+
+public abstract class Shape {
+	public Color color;
+	
+	public Shape(Color color){
+		this.color=color;
+	}
+  abstract public void colorlt();
+}
